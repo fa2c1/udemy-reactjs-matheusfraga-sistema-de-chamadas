4c14cea8b0424bf08ab94de0d717e598a5055e76
Fernando Coutinho <fa2c1@hotmail.com>
2022-03-05 14:16:16 -0300

    Configurações Iniciais

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+let firebaseConfig = {
+
+    apiKey: "AIzaSyBhsf-SFsUJ4XGfrE0MD8uLDsF-sRcDsCo",  
+    authDomain: "sistema-de-chamadas-ee2bd.firebaseapp.com",  
+    projectId: "sistema-de-chamadas-ee2bd",  
+    storageBucket: "sistema-de-chamadas-ee2bd.appspot.com",  
+    messagingSenderId: "727380123521",  
+    appId: "1:727380123521:web:4d0a25c80355b2261d1280",  
+    measurementId: "G-XWQTJBZ5RT"
+  
+  };
+  
+  if(!firebase.app.length){
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  export default firebase;
+  
