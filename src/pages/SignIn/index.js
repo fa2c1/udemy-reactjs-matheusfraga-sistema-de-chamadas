4ab1ca3bd8d5678dc0ab8f2debcdc44d86e0dbde
Fernando Coutinho <fa2c1@hotmail.com>
2022-03-05 16:19:58 -0300

    Criando a  pagina login e cadastro

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,32 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './styles.css';
+import logo from '../../assets/logo.png';
+
 function SignIn() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+    alert('CLICOU')
+  }
+
     return (
-      <div>
-        <h1>SignIn</h1>
+      <div className="container-center">
+        <div className="login">
+          <div className="login-area">
+            <img src={logo} alt='Logo do sistema' />
+          </div>
+          <form onSubmit={handleSubmit}>
+            <h1>Entrar</h1>
+            <input type="text" placeholder='Digite seu e-mail' value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input type="password" placeholder='Digite sua senha' value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button type="submit">Acessar</button>
+          </form>
+          <Link to="/register">Criar uma conta</Link>
+        </div>
       </div>
     );
   }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,38 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../../assets/logo.png';
+
 function SignUp() {
-    return (
-      <div>
-        <h1>SignUp</h1>
-      </div>
-    );
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+    alert('CLICOU')
   }
+
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt='Logo do sistema' />
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <h1>Cadastrar uma conta</h1>
+          <input type="text" placeholder='Digite seu nome' value={nome} onChange={(e) => setNome(e.target.value)} />
+          <input type="text" placeholder='Digite seu email' value={email} onChange={ (e) => setEmail(e.target.value) }/>
+          <input type="password" placeholder='Digite sua senha' value={password} onChange={(e) => setPassword(e.target.value) } />
+          <button type="submit">Cadastrar</button>
+        </form>  
+
+        <Link to="/">Já é cadastrado? Faça seu login</Link>
+      </div>
+    </div>
+  );
+}
   
   export default SignUp;
   
