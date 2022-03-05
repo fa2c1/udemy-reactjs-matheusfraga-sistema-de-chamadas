5e39717f9518304ff0915c3f81a6f4caf5406cd3
Fernando Coutinho <fa2c1@hotmail.com>
2022-03-05 20:08:05 -0300

    Criando a logica de cadastro no Firebase

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,34 +1,81 @@
-import { useState, useEffect, createContext } from 'react';
+
+import { useState, createContext, useEffect } from 'react';
 import firebase from '../services/firebaseConnection';
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }){
-    const [user, setUser] = useState(null);
-    const [loadingAuth, setLoadingAuth] = useState(false);
-    const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [loadingAuth, setLoadingAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(()=>{
-        const storageUser = localStorage.getItem('SistemaUser');
+  useEffect(()=>{
 
-        function loadStorage(){
-            if(storageUser){
-                setUser(JSON.parse(storageUser));
-                setLoading(false);
-            }
+    function loadStorage(){
+      const storageUser = localStorage.getItem('SistemaUser');
+
+      if(storageUser){
+        setUser(JSON.parse(storageUser));
+        setLoading(false);
+      }
+  
+      setLoading(false);
+    }
     
-            setLoading(false);
-        }
+    loadStorage();
 
-        loadStorage()
+  }, [])
 
-    }, [])
+  async function signUp(email, password, nome){
+    setLoadingAuth(true);
 
-    return(
-        <AuthContext.Provider value={{ signed: !!user, user, loading}}>
-            {children}
-        </AuthContext.Provider>
-    )
+    await firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then( async (value)=>{
+      let uid = value.user.uid;
+
+      await firebase.firestore().collection('users')
+      .doc(uid).set({
+        nome: nome,
+        avatarUrl: null,
+      })
+      .then( () => {
+
+        let data = {
+          uid: uid,
+          nome: nome,
+          email: value.user.email,
+          avatarUrl: null
+        };
+
+        setUser(data);
+        storageUser(data);
+        setLoadingAuth(false);
+
+      })
+
+    })
+    .catch((error)=>{
+      console.log(error);
+      setLoadingAuth(false);
+    })
+
+  }
+
+  function storageUser(data){
+    localStorage.setItem('SistemaUser', JSON.stringify(data));
+  }
+
+  async function signOut(){
+    await firebase.auth().signOut();
+    localStorage.removeItem('SistemaUser');
+    setUser(null);
+  }
+
+  return(
+    <AuthContext.Provider value={{ signed: !!user, user, loading, signUp, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  )
 }
 
 export default AuthProvider;
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/auth';
 
 import logo from '../../assets/logo.png';
 
@@ -8,9 +9,15 @@ function SignUp() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const { signUp } = useContext(AuthContext);
+
   function handleSubmit(e){
     e.preventDefault();
-    alert('CLICOU')
+    
+    if(nome !== '' && email !== '' && password !== ''){
+      signUp(email, password, nome)
+    }
+
   }
 
   return (
@@ -23,7 +30,7 @@ function SignUp() {
         <form onSubmit={handleSubmit}>
           <h1>Cadastrar uma conta</h1>
           <input type="text" placeholder='Digite seu nome' value={nome} onChange={(e) => setNome(e.target.value)} />
-          <input type="text" placeholder='Digite seu email' value={email} onChange={ (e) => setEmail(e.target.value) }/>
+          <input type="text" placeholder='Digite seu email' value={email} onChange={(e) => setEmail(e.target.value) }/>
           <input type="password" placeholder='Digite sua senha' value={password} onChange={(e) => setPassword(e.target.value) } />
           <button type="submit">Cadastrar</button>
         </form>  
@@ -34,5 +41,5 @@ function SignUp() {
   );
 }
   
-  export default SignUp;
+export default SignUp;
   
