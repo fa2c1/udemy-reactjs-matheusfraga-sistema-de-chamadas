3a57cbc91893da7e168cf71a38453575867990f7
Fernando Coutinho <fa2c1@hotmail.com>
2022-03-16 19:54:03 -0300

    Criando Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,28 @@
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/auth';
+import { Link } from 'react-router-dom';
+import { FiHome, FiUser, FiSettings } from "react-icons/fi";
+import './styles.css';
+import avatar from '../../assets/avatar.png';
+
+export default function Headers(){
+    const { user } = useContext(AuthContext);
+
+    return(
+        <div className="sidebar">
+            <div>
+                <img src={user.avatarUrl === null ? avatar : user.avatarUrl } alt="Foto avatar" />
+            </div>
+
+            <Link to="/dashboard">
+                <FiHome color="#FFF" size={24} />Chamados
+            </Link>
+            <Link to="/customers">
+                <FiUser color="#FFF" size={24} />Clientes
+            </Link>    
+            <Link to="/profile">
+                <FiSettings color="#FFF" size={24} />Configurações
+            </Link>           
+        </div>
+    )
+}
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,17 @@
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/auth';
+
+import Header from '../../components/Header';
+
 function Dashboard() {
+  const { signOut } = useContext(AuthContext);
+  
     return (
       <div>
-        <h1>Dashboard</h1>
-      </div>
+      <Header/>
+      <h1>PAGINA DASHBOARD</h1>
+      <button onClick={ () => signOut() }>Fazer logout</button>
+    </div>
     );
   }
   
